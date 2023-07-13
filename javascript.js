6003dead5fe935a5d25635716b9313929ee909eb
jisu-usij <133517588+jisu-usij@users.noreply.github.com>
2023-07-12 20:05:08 -0400

    Write pseudocode for calculator functionality
    Psuedocode, once translated to code, should handle
    operator buttons and equal button functionality.

--- javascript.js
+++ javascript.js
@@ -14,8 +14,6 @@ function divide(x, y) {
   return x / y;
 }
 
-let x, y, operator;
-
 function operate(operator, x, y) {
   switch (operator) {
     case "+":
@@ -29,14 +27,42 @@ function operate(operator, x, y) {
   }
 }
 
+let x, y, operator;
+let numberBeingBuilt = "";
+
+/*
+Build up the number as user presses number buttons.
+When operator button is pressed and x is undefined:
+  Store in x the number that has been built up so far.
+  Set numberBeingBuilt to "".
+  Store the operator.
+
+Build up the number as user presses number buttons.
+When operator button is pressed and x is defined:
+  Store in y the number that has been built up so far.
+  Set numberBeingBuilt to "".
+  Operate on x and y using the existing operator and store the result in x.
+  Display the result.
+  Store the operator.
+
+When equal button is pressed:
+  if operator is not defined, do nothing.
+  if operator is defined:
+    if numberBeingBuilt === ""
+      Do nothing???? Or...
+      Show error message that an operand is missing.
+    else
+      Store in y the number that has been built up so far.
+      Operate on x and y using the operator and display that result.
+      Set operator to undefined.
+*/
+
 const display = document.querySelector('.display');
-function appendToDisplay(text) {
-  display.textContent += text;
-}
 
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
-    appendToDisplay(button.textContent);
+    numberBeingBuilt += button.textContent;
+    display.textContent = numberBeingBuilt;
   });
 });
